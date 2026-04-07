2d52f6b08b79d4b81f22084de8dab7949fcfebeb
Youssef Ashraf <youseefashraf54@gmail.com>
2026-04-08 01:57:29 +0200

    Update RBTree.java

--- Trees/src/Trees/RBTree.java
+++ Trees/src/Trees/RBTree.java
@@ -18,7 +18,23 @@ public class RBTree extends AbstractTree<RBNode> {
 
     @Override
     public boolean insert(int val) {
-        return false;
+        RBNode x=root;
+        RBNode y=x;
+        while (x!=Nil) {
+            y=x;
+            if(x.getValue()<val) x=x.right;
+            else if(x.getValue()>val) x=x.left;
+            else return false;
+        }
+        RBNode z=new RBNode(val);
+        z.parent=x;
+        if(y==Nil) root=z;
+        else if(z.getValue()<y.getValue()) y.left=z;
+        else y.right=z;
+        z.color=Red;
+        insertFixUp(z);
+        size++;
+        return true;
     }
 
     @Override
@@ -33,5 +49,39 @@ public class RBTree extends AbstractTree<RBNode> {
     @Override
     protected boolean isNil(RBNode node) {
         return node == Nil;
+    }
+    private void LeftRotation(RBNode node) {
+        if(isNil(node)||isNil(node.right)) return;
+        RBNode res=node.right;
+        node.right=res.left;
+        if(!isNil(res.left)) res.left.parent=node;
+        res.parent=node.parent;
+        if(isNil(node.parent)) root=res;
+        else if(node.parent.left==node) node.parent.left=res;
+        else node.parent.right=res;
+        res.left=node;
+        node.parent=res;
+    }
+    private void RightRotation(RBNode node) {
+            if (isNil(node) || isNil(node.left)) return;
+            RBNode res = node.left;
+            node.left = res.right;
+            if (!isNil(res.right)) {
+                res.right.parent = node;
+            }
+            res.parent = node.parent;
+            if (isNil(node.parent)) {
+                root = res;
+            } else if (node == node.parent.right) {
+                node.parent.right = res;
+            } else {
+                node.parent.left = res;
+            }
+            res.right = node;
+            node.parent = res;
+
+    }
+    private void InsertFixUp(RBNode node) {
+        System.out.println("I wanna sleep");
     }
 }
